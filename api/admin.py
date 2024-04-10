19823bb10845ccd190812a77599af0e70427ec6d
sooj <soojksh@gmail.com>
2024-04-10 12:06:45 +0530

    workflow set

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Student
 
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'roll', 'city']
+
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
